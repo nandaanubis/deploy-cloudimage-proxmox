b5b401ddf44eca495f4440a3e423de6272c60fca
Nanda Anubis <42839965+nandaanubis@users.noreply.github.com>
2022-07-06 14:42:28 +0700

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,51 +1,59 @@
 #!/bin/bash
 set -o errexit
-
 clear
-printf "\n*** This script will download a cloud image and create a Proxmox VM template from it. ***\n\n"
-
+printf "\n*** This script will download a cloud image and create a Proxmox VM from it. ***\n\n"
+## TODO
 ## - verify authenticity of downloaded images using hash or GPG
-
-printf "* Available templates to generate:\n 1) Debian 10\n 2) Debian 11\n 3) Ubuntu 20.04\n\n"
-read -p "* Enter number of distro to use: " OSNR
-
-BRIDGE=vmbr0
-USERCONFIG_DEFAULT=none # cloud-init-config.yml
+printf "* Available templates to generate:\n 1) Debian 11\n 2) Centos 7\n 3) Ubuntu 20.04\n 4) Cloudlinux 8.5 + Cpanel\n\n"
+read -p "* Enter number of distro to use: " OSPROX
 NODEPROX="($hostname)"
 
-case $OSNR in
+
+case $OSPROX in
 
   1)
-    OSNAME=debian10
-    VMID_DEFAULT=910
+    OSNAME=Debian 11
+    VMID_DEFAULT=900
     read -p "Enter a VM ID for $OSNAME [$VMID_DEFAULT]: " VMID
     VMID=${VMID:-$VMID_DEFAULT}
     VMIMAGE=debian-9-openstack-amd64.qcow2
-    NOTE="\n## Default user is 'debian'\n## NOTE: Setting a password via cloud-config does not work.\n"
-    printf "$NOTE\n"
     wget -P /tmp -N https://cdimage.debian.org/cdimage/openstack/current-9/$VMIMAGE
     ;;
 
   2)
-    OSNAME=debian11
-    VMID_DEFAULT=920
+    OSNAME="Centos 7"
+    VMID_DEFAULT=900
     read -p "Enter a VM ID for $OSNAME [$VMID_DEFAULT]: " VMID
     VMID=${VMID:-$VMID_DEFAULT}
-    VMIMAGE=debian-10-openstack-amd64.qcow2
-    NOTE="\n## Default user is 'debian'\n"
-    printf "$NOTE\n"
-    wget -P /tmp -N https://cdimage.debian.org/cdimage/openstack/current-10/$VMIMAGE
+    VMIMAGE=CentOS-7-x86_64-GenericCloud-2111.qcow2
+    wget -P /tmp -N https://cloud.centos.org/centos/7/images/$VMIMAGE
     ;;
 
   3)
-    OSNAME=ubuntu2
-    VMID_DEFAULT=930
+    OSNAME="Ubuntu 20.04"
+    VMID_DEFAULT=900
     read -p "Enter a VM ID for $OSNAME [$VMID_DEFAULT]: " VMID
     VMID=${VMID:-$VMID_DEFAULT}
     VMIMAGE=focal-server-cloudimg-amd64.img
-    NOTE="\n## Default user is 'ubuntu'\n"
-    printf "$NOTE\n"
     wget -P /tmp -N https://cloud-images.ubuntu.com/focal/current/$VMIMAGE
+    ;;
+	
+  4)
+    OSNAME="Cloudlinux 8.5 + Cpanel"
+    VMID_DEFAULT=900
+    read -p "Enter a VM ID for $OSNAME [$VMID_DEFAULT]: " VMID
+    VMID=${VMID:-$VMID_DEFAULT}
+    VMIMAGE=cloudlinux-8.5-x86_64-cpanel-openstack-20220414.qcow2
+    wget -P /tmp -N https://download.cloudlinux.com/cloudlinux/images/$VMIMAGE
+    ;;
+
+   5)
+    OSNAME="Cloudlinux 7.9 + Cpanel"
+    VMID_DEFAULT=900
+    read -p "Enter a VM ID for $OSNAME [$VMID_DEFAULT]: " VMID
+    VMID=${VMID:-$VMID_DEFAULT}
+    VMIMAGE=cloudlinux-7.9-x86_64-cpanel-openstack-20220621.qcow2
+    wget -P /tmp -N https://download.cloudlinux.com/cloudlinux/images/$VMIMAGE
     ;;
 
 
@@ -59,41 +67,45 @@ esac
     && printf "\n** Uncompressing image (waiting to complete...)\n" \
     && bzip2 -d --force /tmp/$VMIMAGE \
     && VMIMAGE=$(echo "${VMIMAGE%.*}") # remove .bz2 file extension from file name
-
-# TODO: could prompt for the VM name
-printf "\n** Creating a VM with $MEMORY MB using network bridge $BRIDGE\n"
-qm create $VMID --name $OSNAME-cloud --memory $MEMORY --net0 virtio,bridge=$BRIDGE
-
-printf "\n** Importing the disk in qcow2 format (as 'Unused Disk 0')\n"
-qm importdisk $VMID /tmp/$VMIMAGE local -format qcow2
-
-printf "\n** Attaching the disk to the vm using VirtIO SCSI\n"
-qm set $VMID --scsihw virtio-scsi-pci --scsi0 /var/lib/vz/images/$VMID/vm-$VMID-disk-0.qcow2
-
-printf "\n** Setting boot and display settings with serial console\n"
-qm set $VMID --boot c --bootdisk scsi0 --serial0 socket --vga serial0
-
-printf "\n** Using a dhcp server on $BRIDGE (or change to static IP)\n"
-qm set $VMID --ipconfig0 ip=dhcp
-
-printf "\n** Creating a cloudinit drive managed by Proxmox\n"
-qm set $VMID --ide2 local:cloudinit
-
-printf "\n** Enable Qemu Guest Agent\n"
-qm set $VMID --agent enabled=1
-
-printf "\n** Enable Qemu Guest Agent\n"
-sleep 30
-printf "\n** Update Size\n"
-qm resize $VMID scsi0 $RESIZE
-
+	
 ## TODO: Also ask for a network configuration. Or create a config with routing for a static IP
-printf "\n*** Create Username and password VM.\n"
+printf "\n*** Config VM .\n"
+read -p ' Input Hostname for Virtual Machine (exp : iwasyourfather.end ) : ' VMNAME
 read -p ' Input your username for Virtual Machine : ' VMUSER
 read -p ' Input your password for Virtual Machine : ' VMPASS
-qm set $VMID --cipassword="$VMPASS" --ciuser=$VMUSER
+read -p ' Input Memory for Virtual Machine (exp: 1024 for 1G ) : ' VMRAM
+read -p ' Input Increase Storage do you want (exp : 20G ): ' VMSTOR
+read -p ' Input Interface Network (exp : vmbr0 ): ' VMNET
 
 
-# The NOTE is added to the Summary section of the VM (TODO there seems to be no 'qm' command for this)
-#printf "#$NOTE\n" >> /etc/pve/nodes/$NODEPROX/qemu-server/$VMID.conf
-printf "$NOTE\n\n"
+printf "\n** Creating a VM with $VMRAM MB using network bridge $VMNET\n"
+qm create $VMID --name $VMNAME --memory $VMRAM --net0 virtio,bridge=$VMNET &> /dev/null
+
+printf "\n** Importing the disk in qcow2 format (as 'Unused Disk 0')\n"
+qm importdisk $VMID /tmp/$VMIMAGE local -format qcow2 &> /dev/null
+
+printf "\n** Attaching the disk to the vm using VirtIO SCSI\n"
+qm set $VMID --scsihw virtio-scsi-pci --scsi0 /var/lib/vz/images/$VMID/vm-$VMID-disk-0.qcow2 &> /dev/null
+
+printf "\n** Setting boot and display settings with serial console\n"
+qm set $VMID --boot c --bootdisk scsi0 --serial0 socket --vga serial0 &> /dev/null
+
+printf "\n** Using a dhcp server on $VMNET\n"
+qm set $VMID --ipconfig0 ip=dhcp &> /dev/null
+
+printf "\n** Creating a cloudinit drive managed by Proxmox\n"
+qm set $VMID --ide2 local:cloudinit &> /dev/null
+
+printf "\n** Enable Qemu Guest Agent\n"
+qm set $VMID --agent enabled=1 &> /dev/null
+
+printf "\n** Create Username and password\n"
+qm set $VMID --cipassword="$VMPASS" --ciuser=$VMUSER &> /dev/null
+printf "\n** Update Size \n"
+sleep 30
+qm resize $VMID scsi0 $VMSTOR &> /dev/null
+
+printf "\n** VM Create status : Done\n"
+echo=""
+printf "\n** Status VM\n"
+qm list | grep $VMID
